d0fe0c9c0874a08b834466f8ff7dceaa58ab4661
obafemi <oludahunsiobafemi18@gmail.com>
2022-06-06 14:38:38 +0100

    styled the Navigation Bar and also created the homepage, sign in and sign up page.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,26 @@ import { ReactComponent as Logo } from "../../assets/DM-catering new Logo.svg";
 import { ReactComponent as DropDown } from "../../assets/bars-solid.svg";
 
 const Header = () => {
+  const handleToggle = () => {};
   return (
     <div className={styles["header-container"]}>
       <div className={styles["logo-container"]}>
         <Logo className={styles.logo} />
       </div>
+      <div className={styles.links}>
+        <ul className={styles.link}>
+          <li>My Cart</li>
+        </ul>
+        <ul className={styles.link}>
+          <li> Order History </li>
+        </ul>
+        <ul className={styles.link}>
+          {" "}
+          <li> Sign In </li>
+        </ul>
+      </div>
       <div className={styles.dropDown}>
-        <DropDown />
+        <DropDown fill="purple" onClick={handleToggle} />
       </div>
     </div>
   );
