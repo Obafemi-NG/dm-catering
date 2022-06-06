44aedb8ea2c70ebc3c7a7ae18b7ca081515d533c
obafemi <oludahunsiobafemi18@gmail.com>
2022-06-07 00:45:17 +0100

    created the cartSlice for redux

---
+++ src/redux/cartSlice.jsx
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const INITIAL_STATE = {
+  cartItems: [],
+  totalQuantity: 0,
+  totalPrice: 0,
+};
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: INITIAL_STATE,
+  reducers: {
+    addToCart(state, action) {},
+    removeItemFromCart(state, action) {},
+  },
+});
+
+export const cartActions = cartSlice.actions;
+export default cartSlice;
